a632e528744a801b50f7de88e4a2e119fcae14ea
akash nagariya <rishabpeangoriya@gmail.com>
2020-11-25 22:21:56 +0530

    first react city tour project

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "INDIA",
+    img: "./img/Auli.jpg",
+    name: "Auli tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 2,
+    city: "INDIA",
+    img: "./img/Kasol.jpg",
+    name: "Kasol",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    city: "INDIA",
+    img: "./img/Manali.jpg",
+    name: "Manali",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    city: "INDIA",
+    img: "./img/Shimla.jpg",
+    name: "Shimla",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
